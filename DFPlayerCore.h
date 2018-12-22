da0c24546f37ebfaea01a88bc510128d69a90a76
Nikolay <dubki4132@mail.ru>
2018-12-22 21:20:17 +0300

    Delay fix

--- DFPlayerCore.h
+++ DFPlayerCore.h
@@ -58,56 +58,56 @@ class DFPlayerCore
 		//////////////////// COMMANDS ////////////////////
 		void PlayNext() const
 		{
-			this->SendData(0x01, 0x00, 0x00, 16);
+			this->SendData(0x01, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void PlayPrevious() const
 		{
-			this->SendData(0x02, 0x00, 0x00, 16);
+			this->SendData(0x02, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void PlayROOT(uint16_t track) const
 		{
-			this->SendData(0x03, highByte(track), lowByte(track), 16);
+			this->SendData(0x03, highByte(track), lowByte(track), 32);
 			
 			return;
 		}
 		
 		void VolumeUp() const
 		{
-			this->SendData(0x04, 0x00, 0x00, 16);
+			this->SendData(0x04, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void VolumeDown() const
 		{
-			this->SendData(0x05, 0x00, 0x00, 16);
+			this->SendData(0x05, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void SetVolume(uint8_t volume) const
 		{
-			this->SendData(0x06, 0x00, volume, 16);
+			this->SendData(0x06, 0x00, volume, 32);
 			
 			return;
 		}
 		
 		void SetEQ(DFPlayerEqualizer eq) const
 		{
-			this->SendData(0x07, 0x00, (uint8_t)eq, 16);
+			this->SendData(0x07, 0x00, (uint8_t)eq, 32);
 			
 			return;
 		}
 		
 		void PlaybackMode(uint8_t mode) const		// По докам тупо повтор трека 1-3000, только откуда?
 		{
-			this->SendData(0x08, 0x00, mode, 16);
+			this->SendData(0x08, 0x00, mode, 32);
 			
 			return;
 		}
@@ -132,21 +132,21 @@ class DFPlayerCore
 		
 		void Play() const
 		{
-			this->SendData(0x0D, 0x00, 0x00, 16);
+			this->SendData(0x0D, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void Pause() const
 		{
-			this->SendData(0x0E, 0x00, 0x00, 16);
+			this->SendData(0x0E, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void PlayNUM(uint8_t folder, uint8_t track) const
 		{
-			this->SendData(0x0F, folder, track, 16);
+			this->SendData(0x0F, folder, track, 32);
 			
 			return;
 		}
@@ -157,14 +157,14 @@ class DFPlayerCore
 		
 		void PlayMP3(uint16_t track) const
 		{
-			this->SendData(0x12, highByte(track), lowByte(track), 16);
+			this->SendData(0x12, highByte(track), lowByte(track), 32);
 			
 			return;
 		}
 		
 		void PlayADVERT(uint16_t track) const
 		{
-			this->SendData(0x13, highByte(track), lowByte(track), 16);
+			this->SendData(0x13, highByte(track), lowByte(track), 32);
 			
 			return;
 		}
@@ -173,14 +173,14 @@ class DFPlayerCore
 		
 		void StopAdvert() const
 		{
-			this->SendData(0x15, 0x00, 0x00, 16);
+			this->SendData(0x15, 0x00, 0x00, 32);
 			
 			return;
 		}
 		
 		void Stop() const
 		{
-			this->SendData(0x16, 0x00, 0x00, 16);
+			this->SendData(0x16, 0x00, 0x00, 32);
 			
 			return;
 		}
@@ -252,6 +252,8 @@ class DFPlayerCore
 			uint16_t crc = 0 - (0xFF + 0x06 + command + 0x00 + msb + lsb);
 			data[7] = highByte(crc);
 			data[8] = lowByte(crc);
+			
+			while(this->IsReadyReceive() == false){ }
 			
 			this->_serial->write(data, 10);
 			
